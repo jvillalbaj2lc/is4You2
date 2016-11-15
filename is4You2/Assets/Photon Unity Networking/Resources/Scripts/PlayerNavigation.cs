08e90e304a9343ae8a35407c717d438afed619d2
Vagabund0 <Carlos.Eduardo.Diaz.Figallo@gmail.com>
2016-11-15 22:34:55 +0100

    Cambio en modo de movimiento y MeshNavegation
    1. Se cambia el modo de movimiento de las teclas WASD a clic con el
    ratón
    2. Se agrega la MeshNavegation para el jugador y sus instancias.

---
+++ is4You2/Assets/Photon Unity Networking/Resources/Scripts/PlayerNavigation.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Com.Afrodita.isForYou2
+{
+    public class PlayerNavigation : Photon.PunBehaviour
+    {
+        private Camera cam;
+        private NavMeshAgent agent;
+        private PhotonView myPhotonView;
+
+        void Start(){
+            cam = GetComponentInChildren<Camera>();
+            agent = this.GetComponent<NavMeshAgent>();
+            myPhotonView = this.GetComponent<PhotonView>();
+            Debug.Log("Inicializada las variables para el Navigation");
+        }
+
+        void Update(){
+
+            if (PhotonNetwork.connected) {
+                if (myPhotonView.isMine)
+                    Walk();
+            }
+            else {
+                Walk();
+            }
+        }
+
+        void Walk() {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    agent.SetDestination(hit.point);
+                    Debug.Log("El player se debe mover en la siguiente dirección" + hit.point.ToString());
+                }
+
+            }
+        }
+    }
+}
